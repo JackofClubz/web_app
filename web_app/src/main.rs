5c0cba811b439005fdc81b1384c13f90c03f68be
JackofClubz <lancetersou@gmail.com>
2022-09-17 15:36:54 +0100

    Main

--- web_app/src/main.rs
+++ web_app/src/main.rs
@@ -4,10 +4,10 @@ use to_do::structs::pending::Pending;
 
 fn main() {
     let done: Done = Done::new("shopping");
-    print!("{}", done.super_struct.title);
-    print!("{}", done.super_struct.status);
+    println!("{}", done.super_struct.title);
+    println!("{}", done.super_struct.status);
     let pending:Pending = Pending::new("laundry");
-    print!("{}", pending.super_struct.title);
-    print!("{}", pending.super_struct.title);
+    println!("{}", pending.super_struct.title);
+    println!("{}", pending.super_struct.status);
 
 }
--- web_app/target/debug/deps/web_app-fa19c80da6dfbcf1.d
+++ web_app/target/debug/deps/web_app-fa19c80da6dfbcf1.d
@@ -1,5 +1,10 @@
-/Users/lance/web_app/web_app/target/debug/deps/web_app-fa19c80da6dfbcf1: src/main.rs
+/Users/lance/web_app/web_app/target/debug/deps/web_app-fa19c80da6dfbcf1: src/main.rs src/to_do/mod.rs src/to_do/structs/mod.rs src/to_do/structs/base.rs src/to_do/structs/done.rs src/to_do/structs/pending.rs
 
-/Users/lance/web_app/web_app/target/debug/deps/web_app-fa19c80da6dfbcf1.d: src/main.rs
+/Users/lance/web_app/web_app/target/debug/deps/web_app-fa19c80da6dfbcf1.d: src/main.rs src/to_do/mod.rs src/to_do/structs/mod.rs src/to_do/structs/base.rs src/to_do/structs/done.rs src/to_do/structs/pending.rs
 
 src/main.rs:
+src/to_do/mod.rs:
+src/to_do/structs/mod.rs:
+src/to_do/structs/base.rs:
+src/to_do/structs/done.rs:
+src/to_do/structs/pending.rs:
