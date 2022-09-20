95c8057a95204964bef7557f7648766df4faaa3e
JackofClubz <lancetersou@gmail.com>
2022-09-20 08:54:46 +0100

    Main removing debug files

--- web_app/target/debug/build/serde_json-fee1b97ba8cf0cb0/build_script_build-fee1b97ba8cf0cb0.d
+++
@@ -1,5 +0,0 @@
-/Users/lance/web_app/web_app/target/debug/build/serde_json-fee1b97ba8cf0cb0/build_script_build-fee1b97ba8cf0cb0: /Users/lance/.cargo/registry/src/github.com-1ecc6299db9ec823/serde_json-1.0.85/build.rs
-
-/Users/lance/web_app/web_app/target/debug/build/serde_json-fee1b97ba8cf0cb0/build_script_build-fee1b97ba8cf0cb0.d: /Users/lance/.cargo/registry/src/github.com-1ecc6299db9ec823/serde_json-1.0.85/build.rs
-
-/Users/lance/.cargo/registry/src/github.com-1ecc6299db9ec823/serde_json-1.0.85/build.rs:
