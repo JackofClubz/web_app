007b6ac3abe1fc4f128972de5395fb6387fb5410
JackofClubz <lancetersou@gmail.com>
2022-09-20 14:49:42 +0100

    Main state to main

--- web_app/src/main.rs
+++ web_app/src/main.rs
@@ -1,4 +1,10 @@
 mod to_do;
+mod state;
+use std::env;
+use std::fs::read;
+use state::{write_to_file, read_file};
+use serde_json::value::Value;
+use serde_json::{Map, json};
 use to_do::ItemTypes;
 use to_do::to_do_factory;
 use to_do::structs::traits::create::Create;
@@ -10,6 +16,13 @@ fn main() {
         ItemTypes::Done(item) => println!("it's a done item with the title: {}", item.super_struct.title)
     }
 
+    let args: Vec<String> = env::args().collect();
+    let status: &String = &args[1];
+    let title: &String = &args[2];
+    let mut state:Map<String, Value> = read_file(String::from("./state.json"));
+    println!("{:?}", state);
+    state.insert(title_to_strin(), json!(status));
+    write_to_file("./state.json", &mut state);
 }
 
 /*
