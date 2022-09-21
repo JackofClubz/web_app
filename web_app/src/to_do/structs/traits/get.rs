44eb606049700c6d2725fdefd43410836e48bb15
JackofClubz <lancetersou@gmail.com>
2022-09-21 06:04:12 +0100

    Main Update get

--- web_app/src/to_do/structs/traits/get.rs
+++ web_app/src/to_do/structs/traits/get.rs
@@ -6,9 +6,9 @@ pub trait Get {
     fn get(&self, title: &String, state:Map<String, Value>) {
         let item: Option<&Value> = state.get(title);
         match item{
-            Some!(result) =>{
-                println!("/n/nItem: {}", title):
-                print!("Status: {} /n/n", result);
+            Some(result) =>{
+                println!("/n/nItem: {}", title);
+                println!("Status: {} /n/n", result);
             },
             None => print!("item: {} was not found", title)
         }
