c4592ecda2d921c81bb29be7fea10966f30a493b
JackofClubz <lancetersou@gmail.com>
2022-09-21 06:01:02 +0100

    Main Updating traits: -get: take the state, call the get function for the map, and then manage that with a match statement and print the outcome.

---
+++ web_app/target/debug/deps/web_app-23f790049ba9ab96.d
@@ -0,0 +1,16 @@
+/Users/lance/web_app/web_app/target/debug/deps/web_app-23f790049ba9ab96.rmeta: src/main.rs src/to_do/mod.rs src/to_do/structs/mod.rs src/to_do/structs/base.rs src/to_do/structs/done.rs src/to_do/structs/pending.rs src/to_do/structs/traits/mod.rs src/to_do/structs/traits/create.rs src/to_do/structs/traits/delete.rs src/to_do/structs/traits/edit.rs src/to_do/structs/traits/get.rs src/state.rs
+
+/Users/lance/web_app/web_app/target/debug/deps/web_app-23f790049ba9ab96.d: src/main.rs src/to_do/mod.rs src/to_do/structs/mod.rs src/to_do/structs/base.rs src/to_do/structs/done.rs src/to_do/structs/pending.rs src/to_do/structs/traits/mod.rs src/to_do/structs/traits/create.rs src/to_do/structs/traits/delete.rs src/to_do/structs/traits/edit.rs src/to_do/structs/traits/get.rs src/state.rs
+
+src/main.rs:
+src/to_do/mod.rs:
+src/to_do/structs/mod.rs:
+src/to_do/structs/base.rs:
+src/to_do/structs/done.rs:
+src/to_do/structs/pending.rs:
+src/to_do/structs/traits/mod.rs:
+src/to_do/structs/traits/create.rs:
+src/to_do/structs/traits/delete.rs:
+src/to_do/structs/traits/edit.rs:
+src/to_do/structs/traits/get.rs:
+src/state.rs:
