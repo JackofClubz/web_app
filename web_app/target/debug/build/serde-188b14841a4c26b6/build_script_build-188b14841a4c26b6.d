3bd965fb02f012fa3f0beb60f11d3532d21d94ac
JackofClubz <lancetersou@gmail.com>
2022-09-21 06:01:53 +0100

    Main deleting wasm, taget

--- web_app/target/debug/build/serde-188b14841a4c26b6/build_script_build-188b14841a4c26b6.d
+++
@@ -1,5 +0,0 @@
-/Users/lance/web_app/web_app/target/debug/build/serde-188b14841a4c26b6/build_script_build-188b14841a4c26b6: /Users/lance/.cargo/registry/src/github.com-1ecc6299db9ec823/serde-1.0.144/build.rs
-
-/Users/lance/web_app/web_app/target/debug/build/serde-188b14841a4c26b6/build_script_build-188b14841a4c26b6.d: /Users/lance/.cargo/registry/src/github.com-1ecc6299db9ec823/serde-1.0.144/build.rs
-
-/Users/lance/.cargo/registry/src/github.com-1ecc6299db9ec823/serde-1.0.144/build.rs:
