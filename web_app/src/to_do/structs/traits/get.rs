f2bf5da9ce546b6a405e501b45e827df1890e07b
JackofClubz <lancetersou@gmail.com>
2022-09-21 06:00:57 +0100

    Main Updating traits: -get: take the state, call the get function for the map, and then manage that with a match statement and print the outcome.

--- web_app/src/to_do/structs/traits/get.rs
+++ web_app/src/to_do/structs/traits/get.rs
@@ -1,9 +1,22 @@
+use serde_json::Map;
+use serde_json::value::Value;
+
 pub trait Get {
 
-    fn get(&self, title: &str) {
-
-        println!("{} is being fetched", title);
+    fn get(&self, title: &String, state:Map<String, Value>) {
+        let item: Option<&Value> = state.get(title);
+        match item{
+            Some!(result) =>{
+                println!("/n/nItem: {}", title):
+                print!("Status: {} /n/n", result);
+            },
+            None => print!("item: {} was not found", title)
+        }
 
     }
 
 }
+
+//we assume that the get trait will require a title,
+// the &self paramater allows the struct to call the 
+// function directly like some_struct.get(&String::from("something"))
