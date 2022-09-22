35def986c0d63982089cb6b910d8f4348a924057
JackofClubz <lancetersou@gmail.com>
2022-09-22 07:13:03 +0100

    Main Update traits

--- web_app/src/to_do/structs/traits/create.rs
+++ web_app/src/to_do/structs/traits/create.rs
@@ -1,9 +1,16 @@
 /*
 items of create, there is only one function and that is to create items. 
+need to update the json with the write_to_file function from our state crate
 */
+use serde_json::Map;
+use serde_json::value::Value;
+use serde_json::json;
+use crate::state::write_to_file;
 
 pub trait Create{
-    fn create(&self, title: &str){
-        println!("{} is being created", title);
+    fn create(&self, title: &str, status: &String, state: &mut Map<String, Value>){
+        state.insert(title.to_string(), json!(status));
+        write_to_file("./state.json", state);
+        println!("\n\n {} is being created \n \n", title);
     }
 }
--- web_app/src/to_do/structs/traits/get.rs
+++ web_app/src/to_do/structs/traits/get.rs
@@ -7,8 +7,8 @@ pub trait Get {
         let item: Option<&Value> = state.get(title);
         match item{
             Some(result) =>{
-                println!("/n/nItem: {}", title);
-                println!("Status: {} /n/n", result);
+                println!("\n\nItem: {}", title);
+                println!("Status: {} \n\n", result);
             },
             None => print!("item: {} was not found", title)
         }
@@ -18,5 +18,7 @@ pub trait Get {
 }
 
 //we assume that the get trait will require a title,
-// the &self paramater allows the struct to call the 
-// function directly like some_struct.get(&String::from("something"))
+// the &self paramater allows the struct to call the function directly 
+//like some_struct.get(&String::from("something"))
+// now we are taking the state, call the get function for the map and then manage
+// that with a match statement and print the outcome
