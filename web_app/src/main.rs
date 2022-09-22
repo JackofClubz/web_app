e0e170eee2afb57e8f82a6c9c0d3a7d8881a20e7
JackofClubz <lancetersou@gmail.com>
2022-09-22 18:44:19 +0100

    Main Final update

--- web_app/src/main.rs
+++ web_app/src/main.rs
@@ -1,42 +1,44 @@
 mod state;
 mod to_do;
+mod processes;
 
 use std::env;
-use state::{write_to_file, read_file};
+use state::read_file;
 use serde_json::value::Value;
-use serde_json::{Map, json};
-
-use to_do::ItemTypes;
+use serde_json::Map;
 use to_do::to_do_factory;
-use to_do::structs::traits::create::Create;
+use processes::process_input;
+
 
 
 fn main() {
 
-    // collects the environment arguments passed in by the user and collects to a vector of strings
+    // collect the arguments passed into the program
     let args: Vec<String> = env::args().collect();
 
-    // defines the commands from the environment
-    let status: &String = &args[1];
+    // define the parameters from the arguments
+    let command: &String = &args[1];
     let title: &String = &args[2];
 
-    // read the json file and print it using the debug notation
-    let mut state: Map<String, Value> = read_file(String::from("./state.json"));
-    println!("{:?}", state);
+    // read the JSON file to get the saved to do items
+    let state: Map<String, Value> = read_file(String::from("./state.json"));
 
-    // insert the title and status into the map
-    state.insert(title.to_string(), json!(status));
+    // define outside the match scope to enable using the status later on
+    let status: String;
 
-    // write the map to the JSON file
-    write_to_file(String::from("./state.json"), &mut state);
-
-
-    let to_do_item: Result<ItemTypes, &'static str> = to_do_factory(
-        &String::from("pending"), String::from("washing")
-    );
-
-    match to_do_item.unwrap() {
-        ItemTypes::Pending(item) => item.create(&item.super_struct.title, &item.super_struct.status, &mut state),
-        ItemTypes::Done(item) => println!("it's a done item with the title: {}", item.super_struct.title)
+    // check to see if the title is already there, setting status to pending if not
+    match &state.get(*&title) {
+        Some(result) => {
+            status = result.to_string().replace('\"', "");
+        }
+        None=> {
+            status = String::from("pending");
+        }
     }
+
+    // create a to do struct depending on the status
+    let item = to_do_factory(&status, title.to_string()).expect(&status);
+
+    // affect the state based on the struct and command passed into the program
+    process_input(item, command.to_string(), &state);
 }
--- web_app/src/processes.rs
+++ web_app/src/processes.rs
@@ -1,5 +1,6 @@
 use serde_json::Map;
 use serde_json::value::Value;
+
 use super::to_do::ItemTypes;
 use super::to_do::structs::done::Done;
 use super::to_do::structs::pending::Pending;
@@ -8,9 +9,19 @@ use super::to_do::structs::traits::create::Create;
 use super::to_do::structs::traits::delete::Delete;
 use super::to_do::structs::traits::edit::Edit;
 
-fn process_pending(item:Pending, command:String, state: &Map<String, Value>){
+
+/// This function processes the command on a pending to do item.
+///
+/// # item
+/// * item (Pending): the to do item to be processed
+/// * command (String): command to be acted on the to do item
+/// * state (&Map<String, Value>): the state of the to do items for the program
+///
+/// # Returns
+/// None
+fn process_pending(item: Pending, command: String, state: &Map<String, Value>) {
     let mut state = state.clone();
-    match command.as_str(){
+    match command.as_str() {
         "get" => item.get(&item.super_struct.title, state),
         "create" => item.create(&item.super_struct.title, &item.super_struct.status, &mut state),
         "delete" => item.delete(&item.super_struct.title, &mut state),
@@ -19,23 +30,38 @@ fn process_pending(item:Pending, command:String, state: &Map<String, Value>){
     }
 }
 
-fn process_done(item:Done, command:String, state: &Map<String, Value>){
+
+/// This function processes the command on a done to do item.
+///
+/// # item
+/// * item (Pending): the to do item to be processed
+/// * command (String): command to be acted on the to do item
+/// * state (&Map<String, Value>): the state of the to do items for the program
+///
+/// # Returns
+/// None
+fn process_done(item: Done, command: String, state: &Map<String, Value>) {
     let mut state = state.clone();
-    match command.as_str(){
+    match command.as_str() {
         "get" => item.get(&item.super_struct.title, state),
         "delete" => item.delete(&item.super_struct.title, &mut state),
         "edit" => item.set_to_pending(&item.super_struct.title, &mut state),
         _ => println!("command: {} not supported", command)
     }
 }
-/*
-Now that we have defined functions that process our to-do structs, 
-we can build an entry point that takes a struct, memory state, 
-and command so we can funnel the struct into the right function:
-*/
 
-pub fn process_input(item:ItemTypes, command:String, state:&Map<String, Value>){
-    match item{
+
+/// This function processes the user input to decide which function to operate.
+///
+/// # Arguments
+/// * item (ItemTypes): one of the to do item types to be processed
+/// * command (String): the command to be acted on the to do item
+/// * state (&Map<String, Value>): the state of the to do item for the program
+///
+/// # Returns
+/// None
+pub fn process_input(item: ItemTypes, command: String, state: &Map<String, Value>) {
+    match item {
         ItemTypes::Pending(item) => process_pending(item, command, state),
         ItemTypes::Done(item) => process_done(item, command, state)
     }
